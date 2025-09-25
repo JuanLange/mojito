c8c6a5daa90afe715c6e460b6c6b2469534c9375
Juan C. Lange <juancarloslange@gmail.com>
2025-09-25 20:41:53 -0300

    Implement Coctails and Contact sections + some corrections

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
+import Menu from "./components/Menu";
+import Contact from "./components/Contact";
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 
@@ -16,6 +19,8 @@ const App = () => {
       <Cocktails />
       <About />
       <Art />
+      <Menu />
+      <Contact />
     </main>
   )
 }
